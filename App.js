e0fd75bff9d97cef022de0becc34c1cb847db266
Bindushri1 <97144855+Bindushri1@users.noreply.github.com>
2022-01-08 21:25:24 +0530

    Quiz Page

--- App.js
+++ App.js
@@ -1,35 +1,16 @@
-import React from "react";
-
+import React from 'react';
 import './App.css';
-
-import { Hair } from "./components/Hair";
-
-import { Body } from "./components/Body";
-
-import { Face } from "./components/Face";
+import TodoList from './components/TodoList';
 
 function App() {
-
-return (
-
-<div className="App">
-
-<header>Choose your treatment</header>
-
-<div className="container">
-
-<Hair/>
-
-<Face />
-
-<Body />
-
-</div>
-
-</div>
-
-);
-
+  return (
+    <div className='todo-app'>
+      <TodoList />
+      <div className='submit-con'>
+      <button className = "button">Submit</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
