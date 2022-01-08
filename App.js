427a48019a59bf9e1295ac6cc89c988d81d3532d
Bindushri1 <97144855+Bindushri1@users.noreply.github.com>
2022-01-08 21:16:51 +0530

    LOGIN FORM

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Form from './Components/Form'
-function App() {
+import './login.css';
+
+function LoginUi() {
   return (
-    <div className="App">
-       <Form/>
+    <div className="main">
+     <div className="sub-main">
+       <div>
+         <div className="imgs">
+           <div className="container-image">
+             {/* <img src={profile} alt="profile" className="profile"/> */}
+
+           </div>
+
+
+         </div>
+         <div>
+           <h1>Login Page</h1>
+           <div>
+             {/* <img src={email} alt="email" className="email"/> */}
+             <input type="text" placeholder="user name" className="name"/>
+           </div>
+           <div className="second-input">
+             {/* <img src={pass} alt="pass" className="email"/> */}
+             <input type="password" placeholder="password" className="name"/>
+           </div>
+          <div className="login-button">
+          <button>Login</button>
+          </div>
+           
+            <p className="link">
+              <a href="">Forgot password ?</a> Or<a href="">Sign Up</a>
+            </p>
+           
+ 
+         </div>
+       </div>
+       
+
+     </div>
     </div>
   );
 }
 
-export default App;
+export default LoginUi;
